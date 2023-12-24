bf712566c8e4802edec09cfac0da18211c91becd
fasuyaaaPNG <dhavinf.a@gmail.com>
2023-12-24 17:53:13 +0700

    login page

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,9 +1,44 @@
+import "./style.css";
+
 export default function login () {
     return (
         <>
-            <h1>
-                ini login
-            </h1>
+            <div className="container">
+      <img src="/assets/LoginRegister/QUANDARY.png" alt="" className="logoAtas" />
+      <div className="content">
+        <div className="info">
+          <p className="register">
+            Login to your account
+          </p>
+        </div>
+        <form action="">
+          <div className="input">
+            <input type="email" placeholder="Email" className="email" />
+            <input type="password" placeholder="Password" className="password" />
+          </div>
+          <div className="submit">
+            <button className="enter">
+              Sign in
+            </button>
+          </div>
+        </form>
+        <div className="google">
+          <a href="" className="withgoogle">
+            <div className="iconback">
+              <img src="/assets/LoginRegister/google.png" alt="" className="icon"/>
+            </div>
+            <button className="with">
+              Continue with Google
+            </button>
+          </a>
+        </div>
+      </div>
+      <div className="notyet">
+        <p>
+            don't have an account yet? <a href="/regist" className="signin">Sign up</a>
+        </p>
+      </div>
+    </div>
         </>
     )
 }
