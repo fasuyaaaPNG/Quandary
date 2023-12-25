25aaf631783aab2b187709f982d17cd023edebe1
fasuyaaaPNG <dhavinf.a@gmail.com>
2023-12-25 21:57:57 +0700

    ygy

--- app/regist/page.tsx
+++ app/regist/page.tsx
@@ -2,9 +2,76 @@
 
 import "./style.css";
 import { useState, useEffect } from 'react';
+import supabase from "../server/supabaseClient";
 
-export default function Regist() {
+const Regist = () => {
+  const [username, setUsername] = useState('')
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [formError, setFormError] = useState('')
+  
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+  
+    if (!username || !email || !password) {
+      setFormError('Please fill all fields');
 
+      const errorContainer = document.getElementById('error');
+      if (errorContainer) {
+        errorContainer.classList.add('error-show');
+      }
+      return;
+    }
+  
+    // Cek apakah email sudah ada dalam database
+    const { data: existingUser, error: existingUserError } = await supabase
+      .from('Users')
+      .select('email')
+      .eq('email', email)
+      .single();
+  
+    if (existingUserError) {
+      console.error('Error checking existing user:', existingUserError.message);
+      setFormError('');
+      return;
+    }
+  
+    if (existingUser) {
+      setFormError('Email already exists');
+  
+      // Add a class to the error container for styling
+      const errorContainer = document.getElementById('error');
+      if (errorContainer) {
+        errorContainer.classList.add('error-show');
+      }
+  
+      return;
+    }
+  
+    // Jika email belum ada, lakukan penyisipan data
+    const { data, error } = await supabase
+      .from('Users')
+      .insert([{ username, email, password }]);
+  
+    if (error) {
+      console.error('Error inserting user:', error.message);
+      setFormError('Please fill in all the fields correctly');
+  
+      // Add a class to the error container for styling
+      const errorContainer = document.querySelector('error');
+      if (errorContainer) {
+        errorContainer.classList.add('error-show');
+      }
+  
+      return;
+    }
+  
+    if (data) {
+      console.log('User inserted successfully:', data);
+      setFormError('');
+    }
+  };
+  
   const [logo, setlogo] = useState('/assets/LoginRegister/QUANDARY.png');
   const [google, setgoogle] = useState('/assets/LoginRegister/google.png');
       
@@ -50,22 +117,25 @@ export default function Regist() {
           Already have an account? <a href="/login" className="signin">Login</a>
         </p>
       </div>
-        <form action="">
+        <form onSubmit={handleSubmit}>
           <div className="input">
-            <input type="text" placeholder="Username" className="username" />
-            <input type="email" placeholder="Email" className="email" />
-            <input type="password" placeholder="Password" className="password" />
+            <input type="text" placeholder="Username" className="username" value={username} onChange={(e) => setUsername(e.target.value)} />
+            <input type="email" placeholder="Email" className="email" value={email} onChange={(e) => setEmail(e.target.value)}/>
+            <input type="password" placeholder="Password" className="password" value={password} onChange={(e) => setPassword(e.target.value)}/>
           </div>
           <div className="submit">
             <button className="enter">
               Sign up
             </button>
           </div>
+          <div id="error">
+            {formError && <p>{formError}</p>}
+          </div>
         </form>
-        <div className="google">
-          <a href="" className="withgoogle">
-            <div className="iconback">
-              <img src={google} alt="" className="icon"/>
+        <div className="google" tabIndex={-1}>
+          <a href="" className="withgoogle" tabIndex={-1}>
+            <div className="iconback" tabIndex={-1}>
+              <img src={google} alt="" tabIndex={-1} className="icon"/>
             </div>
             <button className="with">
               Sign up with Google
@@ -81,3 +151,5 @@ export default function Regist() {
     </div>
   )
 }
+
+export default Regist
