c77b69a5390270889420b6f2cb2820369d4e68cd
fasuyaaaPNG <dhavinf.a@gmail.com>
2023-12-25 15:19:00 +0700

    icon

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,6 +19,7 @@ export default function RootLayout({
        <head>
                 <link rel="preconnect" href="https://fonts.googleapis.com" />
                 <link rel="preconnect" href="https://fonts.gstatic.com" />
+                <link rel="shortcut icon" href="/assets/LoginRegister/Quandary.png" />
                 <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600&display=swap" rel="stylesheet" />
       </head>
       <body className={inter.className}>{children}</body>
