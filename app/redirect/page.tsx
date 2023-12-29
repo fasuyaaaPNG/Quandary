f93539007561d7d1b741053b1894dfcc53de1534
fasuyaaaPNG <dhavinf.a@gmail.com>
2023-12-29 21:41:19 +0700

    redirect mobile

--- app/redirect/page.tsx
+++ app/redirect/page.tsx
@@ -3,7 +3,15 @@ import "./style.css"
 const Redirect = () => {
     return (
         <div className="container">
-            
+            <img src="/assets/Redirect/valid.png" className="logo" alt="" />
+            <div className="text">
+                <p className="textAtas">
+                    Successfully create an account
+                </p>
+                <p className="textBawah">
+                Wait a few moments to return to the login page
+                </p>
+            </div>
         </div>
     )
 }
