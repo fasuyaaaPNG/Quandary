c73fd7d2d04bf679107952515fe5a006a8671a83
fasuyaaaPNG <dhavinf.a@gmail.com>
2023-12-30 22:58:33 +0700

    layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,10 +17,11 @@ export default function RootLayout({
   return (
     <html lang="en">
        <head>
-                <link rel="preconnect" href="https://fonts.googleapis.com" />
-                <link rel="preconnect" href="https://fonts.gstatic.com" />
-                <link rel="shortcut icon" href="/assets/LoginRegister/Quandary.png" />
-                <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600&display=swap" rel="stylesheet" />
+          <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <link rel="shortcut icon" href="/assets/LoginRegister/Quandary.png" />
+          <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600&display=swap" rel="stylesheet" />
       </head>
       <body className={inter.className}>{children}</body>
     </html>
