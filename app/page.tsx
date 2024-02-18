4e42bcebf19a86be53f9fde0c9bba13ba25c0263
fasuyaaaPNG <dhavinf.a@gmail.com>
2024-02-18 23:50:05 +0700

    new

--- app/page.tsx
+++ app/page.tsx
@@ -4,21 +4,21 @@ export default function Home() {
   return (
     <>
       <div className="backHitam">
-        <div className="container1">
-          <div className="headerLogoNav">
-            <img src="/assets/Landing/Logo.svg" className="headerLogo" alt="" />
-            <div className="headerNav">
-              <a className="navHome">
-                Home
-              </a>
-              <a className="navAbout">
-                About
-              </a>  
-              <a className="navLogin">
-                Log in
-              </a>
-            </div>
+        <div className="headerLogoNav">
+          <img src="/assets/Landing/Logo.svg" className="headerLogo" alt="" />
+          <div className="headerNav">
+            <a className="navHome">
+              Home
+            </a>
+            <a className="navAbout">
+              About
+            </a>  
+            <a className="navLogin">
+              Log in
+            </a>
           </div>
+        </div>
+        <div className="container1">
           <div className="content1">
             <div className="content1Title">
               <h1>
@@ -44,6 +44,42 @@ export default function Home() {
                   <h1 className="joinUsText">
                     Join Us
                   </h1>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="garis1">
+          </div>
+          <div className="whiteBackground">
+            <img src="/assets/Landing/kiri.svg" className="garisKiri" alt="" />
+            <img src="/assets/Landing/kanan.svg" className="garisKanan" alt="" />
+            <img src="/assets/Landing/IlustContent2.svg" alt="" className="ilustContent2" />
+            <div className="content2">
+              <div className="content2Judul">
+                <h1>
+                  What Sets <span className="span1">Quandary</span> Apart?
+                </h1>
+              </div>
+              <div className="decorLingkaran">
+                <img src="/assets/Landing/lingkaran1.svg" alt="" className="decorLingkaran1" />
+                <img src="/assets/Landing/lingkaran2.svg" alt="" className="decorLingkaran2" />
+                <img src="/assets/Landing/lingkaran3.svg" alt="" className="decorLingkaran3" />
+              </div>
+              <div className="content2Desk">
+                <div className="content2DeskJudul">
+                  <p>
+                    <b>Quandary</b> isn't just a forum
+                  </p>
+                </div>
+                <div className="content2DeskParaf1">
+                  <p>
+                    It's a dedicated space crafted for the vibrant exchange of ideas, insights, and discussions centered around Indonesia. 
+                  </p>
+                </div>
+                <div className="content2DeskParaf2">
+                  <p>
+                    Quandary aims to be the go-to platform for anyone seeking to explore, understand, and engage in conversations about Indonesia. 
+                  </p>
                 </div>
               </div>
             </div>
