1867635239ee966095c9da331f46886d985459b1
fasuyaaaPNG <dhavinf.a@gmail.com>
2024-03-10 20:31:42 +0700

    update

--- app/main/profile/edit/page.tsx
+++ app/main/profile/edit/page.tsx
@@ -5,21 +5,87 @@ import {FaArrowLeft, FaCheck, FaHouse, FaMagnifyingGlass, FaPlus, FaBell, FaRegU
 import "./style.css"
 
 export default function Edit() {
+    function countTextBio() {
+        const bioInput = document.querySelector<HTMLInputElement>("form[name=form_main] input[name=bio]");
+        const text = bioInput!.value; 
+        document.getElementById('bio')!.innerText = text.length.toString();
+    }
+
+    function countTextName() {
+        const nameInput = document.querySelector<HTMLInputElement>("form[name=form_main] input[name=fullname]");
+        const text = nameInput!.value; 
+        document.getElementById('fullname')!.innerText = text.length.toString();
+    }
+
+    function countTextUsername() {
+        const usernameInput = document.querySelector<HTMLInputElement>("form[name=form_main] input[name=username]");
+        const text = usernameInput!.value; 
+        document.getElementById('username')!.innerText = text.length.toString();
+    }
+
     return (
         <> 
             <div className="background">
                 <div className="profileAtas">
-                    <FaArrowLeft size={20} className="back"/>
+                    <a href="/main/profile" className="back">
+                        <FaArrowLeft size={20} className="back"/>
+                    </a>
                     <h1 className="profile">
                         Edit profile
                     </h1>
-                    <FaCheck size={25} className="centang"/>
                 </div>
-                <label htmlFor="uploadInput">
+                <label className="inputImage" htmlFor="uploadInput">
                     <img src="/assets/main/image1.jpg" alt="" className="profileImage" />
                     <img src="/assets/editProfile/iconImage.png" className="iconProfile" alt="" />
                 </label>
                 <input type="file" id="uploadInput" className="image" style={{ display: "none" }} />
+                <p className="profileDesk">
+                    Change profile picture
+                </p>
+                <form name="form_main" action="/main" method="POST">
+                    <div className="inputNameDiv">
+                        <div className="labelChar">
+                            <label className="inputNameLabel" htmlFor="fullname">
+                                Profile name
+                            </label>
+                            <label className="inputchar" htmlFor="fullname">
+                                <span id="fullname"></span>/30
+                            </label>
+                        </div>
+                        <input maxLength={30} onInput={countTextName} className="inputName" name="fullname" id="fullname" type="text" />
+                    </div>
+                    <div className="inputNameDiv">
+                        <div className="labelChar">
+                            <label className="inputNameLabel" htmlFor="username">
+                               Username
+                            </label>
+                            <label className="inputchar" htmlFor="username">
+                                <span id="username"></span>/10
+                            </label>
+                        </div>
+                        <input onInput={countTextUsername} maxLength={10} className="inputName" name="username" id="username" type="text" />
+                    </div>
+                    <div className="inputNameDiv">
+                        <div className="labelChar">
+                            <label className="inputNameLabel" htmlFor="name">
+                                Bio
+                            </label>
+                            <label className="inputchar" htmlFor="bio">
+                                <span id="bio"></span>/30
+                            </label>
+                        </div>
+                        <input onInput={countTextBio} maxLength={30} className="inputName" name="bio" type="text" />
+                    </div>
+                    <div className="inputNameDiv">
+                        <label className="inputNameLabelCreated">
+                            Created Account at
+                        </label>
+                        <input className="inputName" type="text" />
+                    </div>
+                    <button type="submit" className="submit">
+                        <FaCheck size={25} className="centang"/>
+                    </button>
+                </form>
                 {/* navbar */}
                 <div className="navbar">
                     <motion.div animate={{translateY: 0, opacity: 1}} className="round"></motion.div>
