9767d1bf8dcb87ff5d33602b2ffd9f7c82a8858d
fasuyaaaPNG <dhavinf.a@gmail.com>
2024-04-10 19:03:13 +0700

    newww fixed

--- app/main/user/[id_user]/page.tsx
+++ app/main/user/[id_user]/page.tsx
@@ -432,23 +432,23 @@ export default function UserDetail() {
             }
         };
 
-    const getTimeAgoString = (createdAt: string): string => {
-        const createdDate = new Date(createdAt);
-        const currentDate = new Date();
-        const timeDifference = currentDate.getTime() - createdDate.getTime();
-        const seconds = Math.floor(timeDifference / 1000);
-        const minutes = Math.floor(seconds / 60);
-        const hours = Math.floor(minutes / 60);
-        const days = Math.floor(hours / 24);
+        const getTimeAgoString = (createdAt: string): string => {
+          const createdDate = new Date(createdAt);
+          const currentDate = new Date();
+          const timeDifference = Math.abs(currentDate.getTime() - createdDate.getTime());
+          const seconds = Math.floor(timeDifference / 1000);
+          const minutes = Math.floor(seconds / 60);
+          const hours = Math.floor(minutes / 60);
+          const days = Math.floor(hours / 24);
       
-        if (days > 0) {
-          return `${days} day${days > 1 ? 's' : ''} ago`;
-        } else if (hours > 0) {
-          return `${hours} hour${hours > 1 ? 's' : ''} ago`;
-        } else {
-          return `${minutes} minute${minutes > 1 ? 's' : ''} ago`;
-        }
-    };
+          if (days > 0) {
+              return `${days} day${days > 1 ? 's' : ''} ago`;
+          } else if (hours > 0) {
+              return `${hours} hour${hours > 1 ? 's' : ''} ago`;
+          } else {
+              return `${minutes} minute${minutes > 1 ? 's' : ''} ago`;
+          }
+      };      
 
     useEffect(() => {
         const fetchUserProfile = async () => {
